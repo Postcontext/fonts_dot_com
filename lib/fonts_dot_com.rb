f21597759ae5eeb24832ef04d53d4329d3ae5447
John Friel <friel.john@gmail.com>
2015-10-30 18:31:17 -0400

    Can now add fonts to projects

--- lib/fonts_dot_com.rb
+++ lib/fonts_dot_com.rb
@@ -61,14 +61,40 @@ module FontsDotCom
     #
     ###
 
-    def list_project_fonts(project_id)
+    def list_project_fonts(project_id, options={})
       # `project_id` should be fonts.com's project ID (returned as
       # `ProjectKey` by the API.)
       raise ArgumentError unless project_id
       
+      offset_and_limit = ''
+      if options[:offset] 
+        offset_and_limit += ( '&wfspstart=' + options[:offset].to_s )
+      end
+      if options[:limit]
+        offset_and_limit += ( '&wfsplimit=' + options[:limit].to_s )
+      end
+
+      response = FontsDotCom::Request.fire({
+        message:  "/rest/json/Fonts/?wfspid=#{project_id}#{offset_and_limit}",
+        method:   :get
+      })
+    end
+    
+    def add_font(options)
+      project_id  = options[:project_id]
+      font_id     = options[:font_id]
+      publish     = options.has_key?(:publish) ? options[:publish] : true
+
+      raise ArgumentError unless project_id && font_id
+      
+      data = {
+        wfsfid: font_id
+      }
+
       response = FontsDotCom::Request.fire({
         message:  "/rest/json/Fonts/?wfspid=#{project_id}",
-        method:   :get
+        method:   :post,
+        data:     data
       })
     end
 

381b7d3a761c4f36b7e00fe34bb8882e69c0d4ce
John Friel <friel.john@gmail.com>
2015-10-29 21:16:45 -0400

    let's call methods on FontsDotCom::Config directly

--- lib/fonts_dot_com/config.rb
+++ lib/fonts_dot_com/config.rb
@@ -5,20 +5,22 @@ module FontsDotCom
     
     CONFIG = YAML.load_file('../../config/fonts_dot_com.yml')
 
-    def public_key
-      @public_key ||= CONFIG[:public_key]
-    end
+    class << self
+      def public_key
+        @public_key ||= CONFIG[:public_key]
+      end
 
-    def private_key
-      @private_key ||= CONFIG[:private_key]
-    end
+      def private_key
+        @private_key ||= CONFIG[:private_key]
+      end
 
-    def api_key
-      @api_key ||= CONFIG[:api_key]
-    end
+      def api_key
+        @api_key ||= CONFIG[:api_key]
+      end
 
-    def app_key
-      api_key
+      def app_key
+        api_key
+      end
     end
   end
 end

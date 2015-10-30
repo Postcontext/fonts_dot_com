174c2c9ea5f89a39e4aa1f1558035dbfaf29819e
John Friel <friel.john@gmail.com>
2015-10-30 17:48:50 -0400

    listing projects and deleting projects now works

--- lib/fonts_dot_com.rb
+++ lib/fonts_dot_com.rb
@@ -8,7 +8,17 @@ module FontsDotCom
 
   class << self
 
-    # Such convenience
+    ##
+    # Projects
+    ##
+    
+    # http://www.fonts.com/web-fonts/developers/api/list-projects
+    def list_projects
+      FontsDotCom::Request.fire({
+        message:  '/rest/json/Projects/', # TODO 
+        method:   :get
+      })
+    end
 
     # http://www.fonts.com/web-fonts/developers/api/add-project
     def add_project(name)
@@ -26,6 +36,21 @@ module FontsDotCom
         data:     data
       })
     end
+   
+    # http://www.fonts.com/web-fonts/developers/api/delete-project
+    def delete_project(project_id)
+      raise ArgumentError unless project_id
+      
+      data = {
+        wfsproject_name: name
+      }
+      
+      response = FontsDotCom::Request.fire({
+        message:  "/rest/json/Projects/?wfspid=#{project_id}",
+        method:   :delete
+      })
+    end
+
 
   end
 
